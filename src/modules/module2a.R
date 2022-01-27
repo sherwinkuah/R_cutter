be379a2ae551d5553753fc09b14241319f6a4c52
Sherwin <sherwinkuah@gmail.com>
2022-01-27 19:17:14 +0800

    dev a function and specify plot items in config

--- src/modules/module2a.R
+++ src/modules/module2a.R
@@ -4,20 +4,33 @@ cat("\n========================================================\n")
 print('Entering Module 2a')
 cat("\n========================================================\n")
 
-png("results/rplot_speed_vs_dist.png") 
-p1 <- ggplot(data = cars, aes(speed, dist))+
-  geom_point() +
-  ggtitle('Plot of speed vs distance') 
 
+# function to plot p1
+generate_speed_plot <- function(data, title) {
+  speed <- data$speed
+  dist <- data$speed
+  plot <- ggplot(data, aes(speed,dist))+
+    geom_point() +
+    ggtitle(title)
+  return(plot)
+}
+
+# load module config file
+module_config <- fromJSON(file = "./config/modules/module2a_config.json")
+title <- module_config$inputs$plots$title
+plotpath <- module_config$outputs$speedplot
+# run the function
+png(plotpath) 
+p1 <- generate_speed_plot(data=cars, title)
 print(p1)
 dev.off() 
 
 
-png("results/rplot_hwy_vs_displ.png") 
-p2 <- ggplot(mpg, aes(displ, hwy, colour = class))  + geom_point() +
-  ggtitle('Plot of highway miles vs engine displacement') 
+#png("results/rplot_hwy_vs_displ.png") 
+#p2 <- ggplot(mpg, aes(displ, hwy, colour = class))  + geom_point() +
+#  ggtitle('Plot of highway miles vs engine displacement') 
 
-print(p2)
-dev.off() 
+#print(p2)
+#dev.off() 
 
 cat("\n--------------------------------------------------------------\n")
